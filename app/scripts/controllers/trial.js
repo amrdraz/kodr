672935b8a86cc2bb93ea62c798d9b30a13998a9b
Amr Draz <amr.m.draz@gmail.com>
2014-07-19 14:04:10 +0200

    Client side tests run showing failures and passes, setting a passed variable to true inside the controller
    - decided to remove structur tab and have the TA use it in Tests when he sees fit reducing challenge to jsut contain tests which could have a sturcture.js test in them
    + need to track challenge trial start working on it's CRUD
    + Think of how this affects the user

--- app/scripts/controllers/trial.js
+++ app/scripts/controllers/trial.js
@@ -1,3 +1,5 @@
+var debounce = require('../utils/debounce');
+
 module.exports = App.ChallengeTryController = Em.ObjectController.extend({
     needs: ['challenge'],
     //
@@ -18,11 +20,15 @@ module.exports = App.ChallengeTryController = Em.ObjectController.extend({
         // });
     },
     results: "Run Code to see output",
-    jshint: function(code) {
+
+    jshint: function(code, cb, options) {
+        options = options || {};
+        var console = this.get('console');
+        var sb = options.sandbox || this.get('csandbox');
         JSHINT(code, {
-            "asi":true, // supress simicolon warning
+            "asi": true, // supress simicolon warning
             "boss": true, // supress warning about using assignment inside while condition
-            "eqnull":true,
+            "eqnull": true,
             "expr": true, // you can type random expressions
             "esnext": false,
             "bitwise": true,
@@ -30,29 +36,88 @@ module.exports = App.ChallengeTryController = Em.ObjectController.extend({
             "eqeqeq": true,
             "eqnull": true,
             "immed": false,
-            "latedef": false,
+            "latedef": options.run || false,
             "newcap": false,
             "noarg": true,
-            "undef": false,
+            "undef": options.run || false,
             "strict": false,
             "trailing": false,
             "smarttabs": true,
         });
+        var errors = JSHINT.errors;
+        if (cb) {
+            if (!errors.length) {
+                cb.call(this, code, console, sb);
+            } else {
+                console.Write('Syntax Error line(' + errors[0].line + '): ' + errors[0].reason + '\n', 'error');
+            }
+
+        }
         // debugger;
-        return JSHINT.errors;
+        return errors;
     },
     actions: {
-        run: function() {
-            this.get('controllers.challenge').send('run');
+        run: debounce(function() {
+            var model = this.get('model');
+            var challenge = this.get('model.challenge');
+            var controller = this;
+            var sb = controller.get('sandbox');
+            var Runner = require('../runners/runner');
+            var iframeTemplate = require('../demo/iframe');
+            controller.jshint(model.get('code'), function(code, console, sb) {
+                sb.load(iframeTemplate, function() {
+                    sb.evaljs(Runner.test(code, challenge.get('tests')));
+                });
+            }, {
+                sandbox: sb,
+                run: true
+            });
+
+        }),
+        sandboxLoaded: function(sb) {
+            var that = this;
+            var log = function(msg) {
+                console.log(msg);
+                that.get('console').Write(msg.toString() + '\n');
+            };
+            var writeTest = function(test, pass) {
+                that.get('console').Write(test.title + '\n', pass);
+            };
+            sb.on('error', log);
+            sb.on('test.done', function(report) {
+                var tests = report.tests.length;
+                var passes = report.passes.length;
+                var failures = report.failures.length;
+                var pass = tests === passes;
+                that.get('console').Write("========= Running Submittion " + (pass ? 'Passed' : 'Failed') + " ==========\n", pass ? 'result' : 'error');
+
+                if (passes) {
+                    report.passes.forEach(function(test) {
+                        writeTest(test, 'result');
+                    });
+                    failures && that.get('console').Write('\n-----------------------------------\n\n');
+                }
+
+                if (failures) {
+                    report.failures.forEach(function(test) {
+                        writeTest(test, 'error');
+                    });
+                }
+                that.get('console').Write("==============================================\n", pass ? 'result' : 'error');
+
+                if (tests === passes) {
+                    that.set('passed', true);
+                }
+            });
+            // sb.on('structure.done', log);
+            sb.on('log', log);
+            console.log('loaded sandbox');
         },
         runInConsole: function() {
             this.send('consoleEval', this.get('model.code'));
         },
         consoleEval: function(command) {
-            var console = this.get('console');
-            var sb = this.get('csandbox');
-            var errors = this.jshint(command);
-            if (!errors.length) {
+            this.jshint(command, function(code, console, sb) {
                 sb.evaljs(command, function(error, res) {
                     if (error) {
                         console.Write(error.name + ': ' + error.message + '\n', 'error');
@@ -60,10 +125,7 @@ module.exports = App.ChallengeTryController = Em.ObjectController.extend({
                         console.Write('==> ' + (res !== undefined ? res : "") + '\n', 'result');
                     }
                 });
-            } else {
-                console.Write('Syntax Error line('+errors[0].line+'): ' + errors[0].reason + '\n', 'error');
-            }
-
+            });
         }
     }
 });
