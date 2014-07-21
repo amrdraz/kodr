5ef265e68aa6ce13723bf9f88f612b868da772ea
Amr Draz <amr.m.draz@gmail.com>
2014-07-21 12:29:32 +0200

    Getting Challenge mixin to work

--- app/scripts/controllers/challenge/edit.js
+++ app/scripts/controllers/challenge/edit.js
@@ -1,6 +1,6 @@
 var debounce = require('../../utils/debounce');
-
-module.exports = Em.ObjectController.extend({
+var ChallengeMixin = require('../../mixins/challengeMixin');
+module.exports = Em.ObjectController.extend(ChallengeMixin,{
     needs: ['challenge'],
     actions: {
         run: debounce(function() {
@@ -9,31 +9,15 @@ module.exports = Em.ObjectController.extend({
             var sb = controller.get('sandbox');
             var Runner = require('../../runners/runner');
             var iframeTemplate = require('../../demo/iframe');
-            sb.load(iframeTemplate, function() {
-                var code;
-                if (model.get('structure')) {
-                    code = Runner.structure(model.get('solution'), model.get('structure'), model.get('callbacks'));
-                    sb.evaljs(code);
-                }
-                if (model.get('tests')) {
-                    code = Runner.test(model.get('solution'), model.get('tests'));
-                    sb.evaljs(code);
-                }
+            controller.jshint(model.get('solution'), function(code, console, sb) {
+                sb.load(iframeTemplate, function() {
+                    sb.evaljs(Runner.test(code, model.get('tests')));
+                });
+            }, {
+                sandbox: sb,
+                run: true
             });
         }),
-        sandboxLoaded: function(sb) {
-            var log = function(msg) {
-                console.log(msg);
-            };
-            sb.on('error', log);
-            sb.on('test.done', log);
-            sb.on('structure.done', log);
-            sb.on('log', log);
-            console.log("sandbox loaded");
-        },
-        consoleEval: function(argument) {
-            // body...
-        },
         validate: function() {
             this.send('run');
         },
--- app/scripts/controllers/trial.js
+++ app/scripts/controllers/trial.js
@@ -1,6 +1,6 @@
 var debounce = require('../utils/debounce');
-
-module.exports = App.ChallengeTryController = Em.ObjectController.extend({
+var ChallengeMixin = require('../mixins/challengeMixin');
+module.exports = App.ChallengeTryController = Em.ObjectController.extend(ChallengeMixin, {
     needs: ['challenge'],
     //
     init: function() {
@@ -21,41 +21,6 @@ module.exports = App.ChallengeTryController = Em.ObjectController.extend({
     },
     results: "Run Code to see output",
 
-    jshint: function(code, cb, options) {
-        options = options || {};
-        var console = this.get('console');
-        var sb = options.sandbox || this.get('csandbox');
-        JSHINT(code, {
-            "asi": true, // supress simicolon warning
-            "boss": true, // supress warning about using assignment inside while condition
-            "eqnull": true,
-            "expr": true, // you can type random expressions
-            "esnext": false,
-            "bitwise": true,
-            "curly": false,
-            "eqeqeq": true,
-            "eqnull": true,
-            "immed": false,
-            "latedef": options.run || false,
-            "newcap": false,
-            "noarg": true,
-            "undef": options.run || false,
-            "strict": false,
-            "trailing": false,
-            "smarttabs": true,
-        });
-        var errors = JSHINT.errors;
-        if (cb) {
-            if (!errors.length) {
-                cb.call(this, code, console, sb);
-            } else {
-                console.Write('Syntax Error line(' + errors[0].line + '): ' + errors[0].reason + '\n', 'error');
-            }
-
-        }
-        // debugger;
-        return errors;
-    },
     actions: {
         run: debounce(function() {
             var model = this.get('model');
@@ -73,59 +38,6 @@ module.exports = App.ChallengeTryController = Em.ObjectController.extend({
                 run: true
             });
 
-        }),
-        sandboxLoaded: function(sb) {
-            var that = this;
-            var log = function(msg) {
-                console.log(msg);
-                that.get('console').Write(msg.toString() + '\n');
-            };
-            var writeTest = function(test, pass) {
-                that.get('console').Write(test.title + '\n', pass);
-            };
-            sb.on('error', log);
-            sb.on('test.done', function(report) {
-                var tests = report.tests.length;
-                var passes = report.passes.length;
-                var failures = report.failures.length;
-                var pass = tests === passes;
-                that.get('console').Write("========= Running Submittion " + (pass ? 'Passed' : 'Failed') + " ==========\n", pass ? 'result' : 'error');
-
-                if (passes) {
-                    report.passes.forEach(function(test) {
-                        writeTest(test, 'result');
-                    });
-                    failures && that.get('console').Write('\n-----------------------------------\n\n');
-                }
-
-                if (failures) {
-                    report.failures.forEach(function(test) {
-                        writeTest(test, 'error');
-                    });
-                }
-                that.get('console').Write("==============================================\n", pass ? 'result' : 'error');
-
-                if (tests === passes) {
-                    that.set('passed', true);
-                }
-            });
-            // sb.on('structure.done', log);
-            sb.on('log', log);
-            console.log('loaded sandbox');
-        },
-        runInConsole: function() {
-            this.send('consoleEval', this.get('model.code'));
-        },
-        consoleEval: function(command) {
-            this.jshint(command, function(code, console, sb) {
-                sb.evaljs(command, function(error, res) {
-                    if (error) {
-                        console.Write(error.name + ': ' + error.message + '\n', 'error');
-                    } else {
-                        console.Write('==> ' + (res !== undefined ? res : "") + '\n', 'result');
-                    }
-                });
-            });
-        }
+        })
     }
 });
