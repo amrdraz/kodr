33bea1207eb07b4ac7ebfc4c3108a90ac27f6f7c
Amr Draz <amr.m.draz@gmail.com>
2014-08-23 18:33:23 +0300

    -_- wrong username

--- back/config/prod.js
+++ back/config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     db: {
         // url:'mongodb://kodrodb:learn4ever@ds063919.mongolab.com:63919/kodr'
-        url:process.env.MONGOHQ_URL || 'mongodb://kodrod:learn4ever@kahana.mongohq.com:10090/kodrod'
+        url:process.env.MONGOHQ_URL || 'mongodb://koderod:learn4ever@kahana.mongohq.com:10090/kodrod'
     },
     port:process.env.PORT || 21634
 };
