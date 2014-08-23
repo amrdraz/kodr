96b99379ff65310dc64128099a561e0161b84809
Amr Draz <amr.m.draz@gmail.com>
2014-08-23 18:31:06 +0300

    changed mongodb

--- back/config/prod.js
+++ back/config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     db: {
         // url:'mongodb://kodrodb:learn4ever@ds063919.mongolab.com:63919/kodr'
-        url:process.env.MONGOHQ_URL || 'mongodb://heroku:wWMCUPbGs8YJvrZ_KlOa4XLDqahk8A-5uWsP1AphxMku6GhVIYnVDFjDjjqcBEfavLLfleBS6k9UQVg27BD2TA@kahana.mongohq.com:10025/app28810263'
+        url:process.env.MONGOHQ_URL || 'mongodb://kodrod:learn4ever@kahana.mongohq.com:10090/kodrod'
     },
     port:process.env.PORT || 21634
 };
