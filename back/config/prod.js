f7b53eaa44222c7a8c1e648f6a5e8e2aad8b229e
Amr Draz <amr.m.draz@gmail.com>
2014-12-02 08:29:59 +0200

    made prod run on local mongodb

--- back/config/prod.js
+++ back/config/prod.js
@@ -2,7 +2,8 @@ module.exports = {
     host:'kodr.in',
     db: {
         // url:'mongodb://kodrodb:learn4ever@ds063919.mongolab.com:63919/kodr'
-        url:process.env.MONGOHQ_URL || 'mongodb://koderod:learn4ever@kahana.mongohq.com:10090/kodrod'
+        // url:process.env.MONGOHQ_URL || 'mongodb://koderod:learn4ever@kahana.mongohq.com:10090/kodrod'
+        url: 'localhost/prod'
     },
     port:process.env.PORT || 21634
 };
