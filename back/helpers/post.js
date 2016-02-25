1d155205b5e2953474747c1ba6a06ee60e1b089c
Abkreno <mohamedelhamamsy789@gmail.com>
2016-02-25 15:37:57 +0200

    Adds model and helper for post

---
+++ back/helpers/post.js
@@ -0,0 +1,28 @@
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+var _ = require('lodash');
+var observer = require('../observer');
+
+module.exports = exports = function (schema, options) {
+    var Model = options.model || options;
+    schema.plugin(require('./_common_helper'), options);
+
+    /**
+     * Post Schema pre-save hooks.
+     * on every save, add the date
+     */
+    schema.pre('save', true, function(next, done) {
+        // get the current date
+        var currentDate = new Date();
+
+        // change the updated_at field to current date
+        this.updated_at = currentDate;
+
+        // if created_at doesn't exist, add to that field
+        if (!this.created_at)
+          this.created_at = currentDate;
+
+        next();
+    });
+
+};
---
+++ back/models/post.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Mixed = mongoose.Schema.Types.Mixed;
+
+
+/**
+ * Post Schema.
+ *
+ * @attribute title         String          post title
+ * @attribute text          String          post markdown
+ * @attribute author        [ObjectId]      Id of the User who created the post
+ * @attribute votes         [ObjectId]      Id's of User's who voted the post
+ * @attribute tags          [Tag]           lecture group used for segmentation (should probably abstract these data later)
+ * @attribute created_at    [Date]          the time at which the post was created
+ * @attribute updated_at    [Date]          the last time at which the post was updated
+ *
+ * @type {mongoose.Schema}
+ */
+
+var PostSchema =  new mongoose.schema({
+  title: {
+      type: String,
+  },
+  text: {
+      type: String,
+  },
+  author: {
+      type: ObjectId,
+      ref: 'User'
+  },
+  votes: [{
+      type: ObjectId,
+      ref: 'User'
+  }],
+  created_at: {
+     type:Date
+  },
+  updated_at: {
+     type:Date
+  }
+});
+
+PostSchema.plugin(require('../helpers/post'), 'Post');
+
+var Post = module.exports = mongoose.model('Post', PostSchema);
