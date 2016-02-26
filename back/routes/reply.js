2ae3d0dcdee71a1c0ce2f0365c425dcb94fd73c7
Abkreno <mohamedelhamamsy789@gmail.com>
2016-02-27 01:49:19 +0200

    Allows user to add/delete/edit replies

--- back/routes/reply.js
+++ back/routes/reply.js
@@ -1,6 +1,7 @@
 var Promise = require('bluebird');
 var Post = require('../models/post');
 var Comment = require('../models/comment');
+var Reply = require('../models/reply');
 var ExpiringToken = require('../models/expiringToken');
 var access = require('./access');
 var observer = require('../observer');
@@ -15,7 +16,13 @@ module.exports = function(app, passport) {
    */
 
   app.get('/api/replies/:id',function(req, res, next) {
-
+    Reply.findById(req.params.id, function(err, model) {
+        if (err) return next(err);
+        if (!model) return res.send(404, "Not Found");
+        res.json({
+            reply: model
+        });
+    });
   });
 
   /**
@@ -26,7 +33,71 @@ module.exports = function(app, passport) {
    */
 
   app.post('/api/replies',access.requireRole() ,function(req, res, next) {
-      console.log(req.body);
+    var reply = req.body.reply;
+    reply.author = reply.user || req.user.id;
+    reply.created_at = reply.updated_at = new Date();
+    reply = new Reply(reply);
+    reply.save(function(err,model) {
+        if(err)
+          next(err);
+        res.json({
+          reply: model
+        });
+    });
+  });
+
+  /**
+   * Edit a reply.
+   *
+   * @param   reply id
+   * @returns
+   */
+
+  app.put('/api/replies/:id', access.requireRole(), function(req, res, next) {
+    Reply.findById(req.params.id, function(err, reply) {
+      if (!reply)
+        return next(new Error('Could find the Reply'));
+      else {
+        if(req.user._id.toString()===reply.author.toString()){
+          reply.updated_at = new Date();
+          reply.set(req.body.reply);
+          reply.save(function(err,model) {
+            if (err)
+              next(err);
+            res.json({
+              reply: model
+            });
+          });
+        } else {
+            return res.send(401, "Unauthorized");
+        }
+      }
+    });
+  });
+
+  /**
+   * Delete a reply.
+   *
+   * @param   reply id
+   * @returns
+   */
+
+  app.delete('/api/replies/:id', access.requireRole(), function(req, res, next) {
+    Reply.findById(req.params.id, function(err, reply) {
+      if (!reply)
+        return next(new Error('Could find the Reply'));
+      else {
+        if(req.user._id.toString()===reply.author.toString()){
+            reply.remove(function(err) {
+              if (err)
+                return next(err);
+              res.send(204)
+            });
+        } else {
+            return res.send(401, "Unauthorized");
+        }
+      }
+    });
   });
 
 }
